324b452ee52090d4fb712fe2261343fe92eddc35
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2021-06-17 21:57:36 +0100

    added @Configuration annotation

--- polozilla-source-kafka/src/main/java/com/isharp/ploniexfeed/connect/PoloniexFeedSender.java
+++ polozilla-source-kafka/src/main/java/com/isharp/ploniexfeed/connect/PoloniexFeedSender.java
@@ -4,9 +4,11 @@ import org.apache.kafka.streams.KeyValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Supplier;
 
+@Configuration
 public class PoloniexFeedSender {
     private static final Logger logger = LoggerFactory.getLogger(PoloniexFeedSender.class);
 
