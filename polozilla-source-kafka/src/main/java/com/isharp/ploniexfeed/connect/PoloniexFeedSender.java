7ae258bc028957da56d01415b9c12e4055b1ecf3
jeremy ward <jeremy_c_ward@yahoo.co.uk>
2021-06-18 21:09:27 +0100

    hooked up listener removed redundant bean declaration

--- polozilla-source-kafka/src/main/java/com/isharp/ploniexfeed/connect/PoloniexFeedSender.java
+++ polozilla-source-kafka/src/main/java/com/isharp/ploniexfeed/connect/PoloniexFeedSender.java
@@ -3,6 +3,7 @@ package com.isharp.ploniexfeed.connect;
 import org.apache.kafka.streams.KeyValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +14,8 @@ public class PoloniexFeedSender {
     private static final Logger logger = LoggerFactory.getLogger(PoloniexFeedSender.class);
 
 
-   private final WebsocketLIstener websocketLIstener = new WebsocketLIstener();
+   @Autowired
+    private WebsocketLIstener websocketLIstener = new WebsocketLIstener();
 
 
 
@@ -21,10 +23,6 @@ public class PoloniexFeedSender {
         logger.info("Starting up feed sender");
     }
 
-    @Bean
-    public WebsocketLIstener getWebsocketLIstener() {
-        return websocketLIstener;
-    }
 
     @Bean
     public Supplier<KeyValue<String,String>> poloniexRawMessageRecieved() {
